c69bb85cdd0f05298ed3f2a49a617d8b800242b2
Duong Minh Quy <minhquy43123@gmail.com>
2025-08-05 19:35:37 +0700

    netsafe

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          lexend: ['__Lexend_86777a', '__Lexend_Fallback_8677', 'sans-serif'],
+        },
+        
+      },
+    },
+    plugins: [],
+  };
